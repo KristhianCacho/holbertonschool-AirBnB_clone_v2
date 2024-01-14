f856f9a4853bff01b82230b6ac00706436ada3a6
KristhianCacho <kristhiancacho518@gmail.com>
2024-01-14 09:30:52 -0800

    setup_mysql_test.sql

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,6 +1,11 @@
--- Prepares a MySQL server for the project
--- Create database and User
+-- Create the database
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+
+-- create new user
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+
+-- Grant all privileges
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+
+-- Grant SELECT privilege
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
