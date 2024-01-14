464cc28dce9c562f81ffe06e19a0408ab01fa70e
KristhianCacho <kristhiancacho518@gmail.com>
2024-01-14 09:43:21 -0800

    test_amenity.py

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -2,7 +2,8 @@
 """ """
 from tests.test_models.test_base_model import test_basemodel
 from models.amenity import Amenity
-
+from os import environ
+import unittest
 
 class test_Amenity(test_basemodel):
     """ """
@@ -17,3 +18,13 @@ class test_Amenity(test_basemodel):
         """ """
         new = self.value()
         self.assertEqual(type(new.name), str)
+
+    @unittest.skipIf(environ.get('HBNB_TYPE_STORAGE') != 'db', "test for DBStorage")
+    def test_name2(self):
+        """ """
+        new = self.value()
+        self.assertEqual(type(new.name), str)
+
+
+if __name__ == '__main__':
+    unittest.main()
