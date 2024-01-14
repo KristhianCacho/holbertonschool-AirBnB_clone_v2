9b60f8735610acb8be48473abeea60eeb4671269
KristhianCacho <kristhiancacho518@gmail.com>
2024-01-14 11:40:18 -0800

    state.py

--- models/state.py
+++ models/state.py
@@ -1,25 +1,29 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
 from models.city import City
-import models
+from os import environ
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
 
-class State(BaseModel, Base):
-    """ State class """
-    __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
-    cities = relationship("City",
-                          backref='state', cascade="all, delete")
+if environ.get('HBNB_TYPE_STORAGE') == 'db':
+    class State(BaseModel, Base):
+        """ State class for DB"""
+        __tablename__ = "states"
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', backref='states', cascade='all, delete')
 
-    @property
-    def cities(self):
-        """ Getter for cities """
-        l = []
-        dic = models.storage.all(City)
-        for city in dic.values():
-            if city.state_id == self.id:
-                l.append(city)
-        return l
+else:
+    class State(BaseModel):
+        """State class for FS"""
+        name = ''
+
+        @property
+        def cities(self):
+            """Returns the list of City instances"""
+            from models import storage
+            return[city
+                   for city in storage.all(City).values()
+                   if city.state_id == self.id]
